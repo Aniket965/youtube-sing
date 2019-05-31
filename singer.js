72e52a9b6acca2547e0fbfa0a825e3f5979de111
aniket965 <aniket965.as@gmail.com>
2019-05-31 10:15:14 +0530

    fixes:issues with macos

--- singer.js
+++ singer.js
@@ -7,12 +7,7 @@ const stream = require("youtube-audio-stream");
 const decoder = require("lame").Decoder;
 const Speaker = require("speaker");
 var ytpl = require("ytpl");
-
-const speaker = new Speaker({
-    channels: 2,          // 2 channels
-    bitDepth: 16,         // 16-bit samples
-    sampleRate: 44100     // 44,100 Hz sample rate
-  });
+const log = require("emojifylogs").log;
 
 module.exports = url => {
   var path = process.cwd();
@@ -24,53 +19,66 @@ module.exports = url => {
     ytpl(url.substring(i), function(err, playlist) {
       if (err) throw err;
       playlist.items.forEach(item => {
-        if (!IsRunning) {
-          playsong(item.url_simple);
-        } else {
-          Playlist_urls.push(item.url_simple);
-        }
+        Playlist_urls.push(item.url_simple);
       });
+      playsong(Playlist_urls[0]);
     });
   } else {
     playsong(url);
   }
   function playsong(url) {
+    let streamas;
+    let speaker;
     IsRunning = true;
     youtubedl.getInfo(url, function(err, info) {
       if (err) throw err;
-      console.log("Playing...\n".cyan + info.title.rainbow);
-      console.log("wanna save offline ?(yes)".yellow);
+      log("Playing...\n".cyan + info.title.green);
+      log.info("wanna save offline ? (yes) or wanna skip (s) ");
       var stdin = process.openStdin();
-
       stdin.addListener("data", function(d) {
         if (d.toString().trim() === "yes") {
-          console.log("downloading....");
+          log("downloading....");
           exec(
             "youtube-dl --extract-audio --audio-format mp3 " + url,
             (err, stdout, stderr) => {
               if (err) {
-                console.error(err);
+                error(err);
               } else {
-                console.log("Downloaded 🔥".yellow);
+                log.info("Downloaded 🔥".yellow);
               }
             }
           );
         }
-      });
-
-      let streamas = stream(url)
-        .pipe(decoder())
-        .pipe(speaker);
-      streamas.on("open", () => (IsRunning = true));
-      streamas.on("close", () => {
-        IsRunning = false;
-        stdin.removeAllListeners();
-        if (Playlist_urls.length > 0) {
-          Playlist_urls.shift();
-          playsong(Playlist_urls[0]);
+        if (d.toString().trim() === "s") {
+          log.info('Skipping the current Song'.bgMagenta)
+          streamas.destroy();
+          IsRunning = false;
+          stdin.removeAllListeners();
         }
       });
-    });
 
+      try {
+        speaker = new Speaker({
+          channels: 2, // 2 channels
+          bitDepth: 16, // 16-bit samples
+          sampleRate: 44100 // 44,100 Hz sample rate
+        });
+        streamas = stream(url)
+          .pipe(decoder())
+          .pipe(speaker);
+        streamas.on("open", () => (IsRunning = true));
+        streamas.on("close", () => {
+          IsRunning = false;
+          speaker.close();
+          stdin.removeAllListeners();
+          if (Playlist_urls.length > 0) {
+            Playlist_urls.shift();
+            playsong(Playlist_urls[0]);
+          } else log("Your Song/Playlisted Ended!".blue);
+        });
+      } catch (e) {
+        log(e);
+      }
+    });
   }
 };
